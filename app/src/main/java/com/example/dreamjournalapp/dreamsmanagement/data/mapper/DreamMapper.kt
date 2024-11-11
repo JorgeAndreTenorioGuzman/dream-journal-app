40209352870b4687d297ce8894f9f9e1df6df7ba
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-11-11 09:33:34 -0600

    Make mapper functions to extension functions

--- app/src/main/java/com/example/dreamjournalapp/dreamsmanagement/data/mapper/DreamMapper.kt
+++ app/src/main/java/com/example/dreamjournalapp/dreamsmanagement/data/mapper/DreamMapper.kt
@@ -3,33 +3,26 @@ package com.example.dreamjournalapp.dreamsmanagement.data.mapper
 import com.example.dreamjournalapp.dreamsmanagement.data.model.DreamEntity
 import com.example.dreamjournalapp.dreamsmanagement.domain.model.DreamDomainModel
 
-class DreamMapper {
+object DreamMapper {
 
-    fun mapToDomain(dreamEntity: DreamEntity): DreamDomainModel {
+    fun DreamEntity.toDomainModel(): DreamDomainModel {
         return DreamDomainModel(
-            id = dreamEntity.id,
-            title = dreamEntity.title,
-            description = dreamEntity.description,
-            date = dreamEntity.date,
-            imageUri = dreamEntity.imageUri
+            id = this.id,
+            title = this.title,
+            date = this.date,
+            imageUri = this.imageUri,
+            description = this.description,
         )
     }
 
-    fun mapToEntity(dreamDomainModel: DreamDomainModel): DreamEntity {
+    fun DreamDomainModel.toDreamEntity(): DreamEntity {
         return DreamEntity(
-            id = dreamDomainModel.id,
-            title = dreamDomainModel.title,
-            description = dreamDomainModel.description,
-            date = dreamDomainModel.date,
-            imageUri = dreamDomainModel.imageUri
+            id = this.id,
+            title = this.title,
+            description = this.description,
+            imageUri = this.imageUri,
+            date = this.date
         )
     }
 
-    fun mapToDomainList(dreamEntities: List<DreamEntity>): List<DreamDomainModel> {
-        return dreamEntities.map { mapToDomain(it) }
-    }
-
-    fun mapToEntityList(dreamDomainModel: List<DreamDomainModel>): List<DreamEntity> {
-        return dreamDomainModel.map { mapToEntity(it) }
-    }
 }
