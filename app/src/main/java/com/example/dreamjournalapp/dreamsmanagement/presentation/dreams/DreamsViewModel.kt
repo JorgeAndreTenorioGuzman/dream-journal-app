01e0dc48a06f2355364188f6713fcdbcedf78628
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-11-11 10:21:11 -0600

    Set state hold by view model and create function to get dreams using the uses cases

--- app/src/main/java/com/example/dreamjournalapp/dreamsmanagement/presentation/dreams/DreamsViewModel.kt
+++ app/src/main/java/com/example/dreamjournalapp/dreamsmanagement/presentation/dreams/DreamsViewModel.kt
@@ -3,10 +3,13 @@ package com.example.dreamjournalapp.dreamsmanagement.presentation.dreams
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.dreamjournalapp.dreamsmanagement.data.model.DreamEntity
 import com.example.dreamjournalapp.dreamsmanagement.domain.model.DreamDomainModel
 import com.example.dreamjournalapp.dreamsmanagement.domain.use_case.DreamUsesCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,10 +17,20 @@ class DreamsViewModel @Inject constructor(
     private val dreamUsesCases: DreamUsesCases
 ): ViewModel() {
 
-    init {
+    private val _state = mutableStateOf(DreamsState())
+    val state: State<DreamsState> = _state
 
+    init {
+        getDreams()
     }
 
-
+    private fun getDreams() {
+        dreamUsesCases.getDreams()
+            .onEach { dreams ->
+                _state.value = state.value.copy(
+                    dreams = dreams
+                )
+            }.launchIn(viewModelScope)
+    }
 
 }
