117198a4d355f11fe2c0d50bfb1d0d3bba91ded5
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-11-11 10:19:23 -0600

    change return type of GetDreamsUseCase and GetDreamsUseCase to use DreamDomainModel

--- app/src/main/java/com/example/dreamjournalapp/dreamsmanagement/domain/use_case/GetDreamUseCase.kt
+++ app/src/main/java/com/example/dreamjournalapp/dreamsmanagement/domain/use_case/GetDreamUseCase.kt
@@ -1,6 +1,7 @@
 package com.example.dreamjournalapp.dreamsmanagement.domain.use_case
 
 import com.example.dreamjournalapp.dreamsmanagement.data.model.DreamEntity
+import com.example.dreamjournalapp.dreamsmanagement.domain.model.DreamDomainModel
 import com.example.dreamjournalapp.dreamsmanagement.domain.repository.DreamRepository
 import kotlinx.coroutines.flow.Flow
 
@@ -8,7 +9,7 @@ class GetDreamUseCase(
     private val repository: DreamRepository
 ) {
 
-    suspend operator fun invoke(id: Int): DreamEntity?{
+    suspend operator fun invoke(id: Int): DreamDomainModel?{
         return repository.getDreamById(id)
     }
 }
