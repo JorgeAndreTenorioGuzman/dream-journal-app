ddb9fb2c52609a69fee7c12f046d9c7cdd1c2620
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-11-13 09:59:45 -0600

    setup compose navigation in MainActivity

--- app/src/main/java/com/example/dreamjournalapp/MainActivity.kt
+++ app/src/main/java/com/example/dreamjournalapp/MainActivity.kt
@@ -11,6 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.dreamjournalapp.dreamsmanagement.presentation.add_edit_dream.AddEditDreamsScreen
+import com.example.dreamjournalapp.dreamsmanagement.presentation.dreams.DreamsScreen
+import com.example.dreamjournalapp.dreamsmanagement.presentation.util.Screen
 import com.example.dreamjournalapp.ui.theme.DreamJournalAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +25,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DreamJournalAppTheme {
-
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.DreamsScreen.route
+                ) {
+                    composable(route = Screen.DreamsScreen.route){
+                        DreamsScreen()
+                    }
+                    composable(route = Screen.AddEditNoteScreen.route) {
+                        AddEditDreamsScreen()
+                    }
+                }
             }
         }
     }
