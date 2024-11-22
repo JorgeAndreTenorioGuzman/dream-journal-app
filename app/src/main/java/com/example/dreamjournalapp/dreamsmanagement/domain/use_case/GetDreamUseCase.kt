d2a0af1ade6689fdadb4472f43ebf0ae79a5af8d
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-11-22 13:08:20 -0600

    add exception logic to GetDreamUseCase

--- app/src/main/java/com/example/dreamjournalapp/dreamsmanagement/domain/use_case/GetDreamUseCase.kt
+++ app/src/main/java/com/example/dreamjournalapp/dreamsmanagement/domain/use_case/GetDreamUseCase.kt
@@ -2,14 +2,22 @@ package com.example.dreamjournalapp.dreamsmanagement.domain.use_case
 
 import com.example.dreamjournalapp.dreamsmanagement.data.model.DreamEntity
 import com.example.dreamjournalapp.dreamsmanagement.domain.model.DreamDomainModel
+import com.example.dreamjournalapp.dreamsmanagement.domain.model.DreamNotFoundException
+import com.example.dreamjournalapp.dreamsmanagement.domain.model.InvalidDreamException
 import com.example.dreamjournalapp.dreamsmanagement.domain.repository.DreamRepository
 import kotlinx.coroutines.flow.Flow
+import kotlin.jvm.Throws
 
 class GetDreamUseCase(
     private val repository: DreamRepository
 ) {
 
+    @Throws(DreamNotFoundException::class)
     suspend operator fun invoke(id: Int): DreamDomainModel?{
-        return repository.getDreamById(id)
+        val dream = repository.getDreamById(id)
+        if(dream == null) {
+            throw DreamNotFoundException("Dream with ID $id not found")
+        }
+        return dream
     }
 }
