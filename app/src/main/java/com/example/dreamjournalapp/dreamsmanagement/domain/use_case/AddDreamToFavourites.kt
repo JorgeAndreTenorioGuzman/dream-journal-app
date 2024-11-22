15d0f09e25773b2fe993dfdffe3ce85ee9dc54b7
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-11-22 10:08:43 -0600

    add Business logic to AddDreamToFavourites use case

--- app/src/main/java/com/example/dreamjournalapp/dreamsmanagement/domain/use_case/AddDreamToFavourites.kt
+++ app/src/main/java/com/example/dreamjournalapp/dreamsmanagement/domain/use_case/AddDreamToFavourites.kt
@@ -1,11 +1,25 @@
 package com.example.dreamjournalapp.dreamsmanagement.domain.use_case
 
+import com.example.dreamjournalapp.dreamsmanagement.domain.model.InvalidDreamException
 import com.example.dreamjournalapp.dreamsmanagement.domain.repository.DreamRepository
 
 class AddDreamToFavourites(
     private val repository: DreamRepository
 ) {
+    @Throws(InvalidDreamException::class)
     suspend operator fun invoke(dreamId: Int, isFavorite: Boolean){
+        if(dreamId == null){
+            throw InvalidDreamException("Dream Id cannot be null")
+        }
+        if(dreamId <= 0){
+            throw InvalidDreamException("Dream id must be positive number")
+        }
+
+        val currentDream = repository.getDreamById(dreamId)
+        if(currentDream != null && currentDream.isFavorite == isFavorite){
+            return
+        }
+
         repository.addDreamToFavourites(dreamId, isFavorite)
     }
 }
