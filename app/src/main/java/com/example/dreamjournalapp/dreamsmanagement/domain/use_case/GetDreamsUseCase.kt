dd8e8a5379b122604724491f8e4ec9d26142358c
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-11-22 10:58:40 -0600

    add business logic to GetDreamsUseCase

--- app/src/main/java/com/example/dreamjournalapp/dreamsmanagement/domain/use_case/GetDreamsUseCase.kt
+++ app/src/main/java/com/example/dreamjournalapp/dreamsmanagement/domain/use_case/GetDreamsUseCase.kt
@@ -4,12 +4,16 @@ import com.example.dreamjournalapp.dreamsmanagement.data.model.DreamEntity
 import com.example.dreamjournalapp.dreamsmanagement.domain.model.DreamDomainModel
 import com.example.dreamjournalapp.dreamsmanagement.domain.repository.DreamRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
 
 class GetDreamsUseCase(
     private val repository: DreamRepository
 ) {
     operator fun invoke(): Flow<List<DreamDomainModel>>{
         return repository.getDreams()
+            .onStart { emit(emptyList()) }
+            .catch { emit(emptyList())}
     }
 
 }
